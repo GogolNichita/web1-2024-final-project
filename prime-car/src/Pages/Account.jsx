3b50161c21ad75703ef77a130aa28498d198800a
GogolNichita <nichitagogol@gmail.com>
2024-02-28 16:14:29 +0200

    Routing and structure init

---
+++ prime-car/src/Pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+
+export default function Account() {
+    return (
+      <>
+          <NavBar/>
+          <h2>ACCOUNT</h2>
+          <Footer/>
+      </>
+    );
+}
---
+++ prime-car/src/Pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+
+export default function MainPage() {
+    return (
+        <>
+            <NavBar/>
+            <h2>MAIN PAGE</h2>
+            <Footer/>
+        </>
+    );
+}
