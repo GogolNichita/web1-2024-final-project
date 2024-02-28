0055f6380cd726b49a3ea62fa45ef8d931a70e41
GogolNichita <nichitagogol@gmail.com>
2024-02-28 16:29:43 +0200

    NavBar added

--- prime-car/src/Components/NavBar.jsx
+++ prime-car/src/Components/NavBar.jsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import "./NavBar.css";
 export default function NavBar() {
-    return(
+    return (
         <>
-
+            <nav>
+                <span>PRIME CAR</span>
+                <ul className="navbutton">
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <a href="#about-us">About</a>
+                    </li>
+                    <li>
+                        <a href="#benefits">Benefits</a>
+                    </li>
+                    <li>
+                        <a href="#cars">Cars</a>
+                    </li>
+                </ul>
+                <ul className="button">
+                    <li>
+                        <a href="#contact-us">Contact Us</a>
+                    </li>
+                    <li>
+                        <Link to="/account">Account</Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     );
 }
