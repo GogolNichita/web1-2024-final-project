d67e4d94903c0840f9be49d433bc059e88bc2c72
GogolNichita <nichitagogol@gmail.com>
2024-02-28 17:27:53 +0200

    NavBar css redacted + structure redacted

--- prime-car/src/Pages/MainPage.jsx
+++ prime-car/src/Pages/MainPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import NavBar from "../Components/NavBar";
-import Footer from "../Components/Footer";
+import NavBar from "../Components/Layout-Components/NavBar";
+import Footer from "../Components/Layout-Components/Footer";
 
 export default function MainPage() {
     return (
