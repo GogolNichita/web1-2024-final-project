e7afc3dc5f35e10454e11db1344030628f30f77f
GogolNichita <nichitagogol@gmail.com>
2024-03-04 12:55:55 +0200

    Header section and css has been added

--- prime-car/src/Components/Main-Components/Header.jsx
+++ prime-car/src/Components/Main-Components/Header.jsx
@@ -4,8 +4,16 @@ import "./Header.css";
 
 export default function (){
     return(
-      <div className="header">
+        <div className="header">
             <NavBar account={false}/>
-      </div>
+            <div className="headerText">
+                <h1>
+                    More economy, same speed
+                </h1>
+                <h3>The most beloved cars of the moment for those who want to ride in style without spending too
+                    much</h3>
+                <button><a href="#cars"> Discover <img src="img/Vector.svg" alt=""/></a></button>
+            </div>
+        </div>
     );
 }
--- prime-car/src/Pages/Account.jsx
+++ prime-car/src/Pages/Account.jsx
@@ -4,10 +4,10 @@ import Footer from "../Components/Layout-Components/Footer";
 
 export default function Account() {
     return (
-      <>
+      <div className="background">
           <NavBar account={true}/>
           <h2>ACCOUNT</h2>
           <Footer/>
-      </>
+      </div>
     );
 }
